7479265b930c1f44ff1b3cc446667bb6325e3397
april ma <Aprilmayi@aprils-iMac.local>
2020-09-27 22:32:00 -0700

    hello world

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>HELLLLLO</h1>
+        <h2>world</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
