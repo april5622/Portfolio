16e2595f321dd895e60cae3c4417f11ca202de50
april ma <Aprilmayi@aprils-iMac.local>
2020-09-27 23:12:53 -0700

    new save

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>HELLLLLO</h1>
-        <h2>world</h2>
+        <Container>
+          <Row>
+            <Col className="columns">About</Col>
+            <Col className="columns">Projects</Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
