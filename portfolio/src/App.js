186c7a39ca785ca814ad87626ac11f5cae19e362
April Ma <54913304+april5622@users.noreply.github.com>
2020-10-24 12:14:19 -0700

    adding profile image

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import pic from "./img/pic.png"
 
 
 function App() {
@@ -10,10 +11,12 @@ function App() {
       <div className="bigBox">
         <div className='leftBox'>
             <h1>Left Box</h1>
-                <a href="https://github.com/april5622/"
-                  className="github icon">
+              <img classname="pic" src={pic} alt="pic"/>
+              <div>
+                <a href="https://github.com/april5622/" className="github icon">
                   <FontAwesomeIcon icon={faGithub} size="2x" />
                 </a>
+              </div>
         </div>
         <div className="rightBox">
             <h1>About</h1>
