089b4bb6e21a74a70645b128e50200ed91847f56
April Ma <54913304+april5622@users.noreply.github.com>
2020-10-26 16:55:53 -0700

    picked font and color

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,18 +5,21 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import pic from "./img/pic.png"
 
 
+
 function App() {
   return (
     <div className="App">
       <div className="bigBox">
         <div className='leftBox'>
-            <h1>Left Box</h1>
-              <img classname="pic" src={pic} alt="pic"/>
-              <div>
-                <a href="https://github.com/april5622/" className="github icon">
-                  <FontAwesomeIcon icon={faGithub} size="2x" />
-                </a>
-              </div>
+          <div className="text-wrap">
+            <h1 className="april">April</h1>
+          </div>
+          <img classname="pic" src={pic} alt="pic"/>
+          <div className="icon-div">
+            <a href="https://github.com/april5622/" className="github icon">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+          </div>
         </div>
         <div className="rightBox">
             <h1>About</h1>
