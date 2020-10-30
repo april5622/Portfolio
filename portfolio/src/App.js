ce7fd0a248f3c29db3db7359cbef1e5e70bb038e
April Ma <54913304+april5622@users.noreply.github.com>
2020-10-30 14:30:11 -0700

    created routes for separate home and project pages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,30 +1,28 @@
 import React from 'react';
 import './App.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import pic from "./img/pic.png"
-
-
+import ProjectsPage from './components/ProjectsPage';
+import HomePage from './components/HomePage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div className="bigBox">
-        <div className='leftBox'>
-          <div className="text-wrap">
-            <h1 className="april">April</h1>
-          </div>
-          <img classname="pic" src={pic} alt="pic"/>
-          <div className="icon-div">
-            <a href="https://github.com/april5622/" className="github icon">
-              <FontAwesomeIcon icon={faGithub} size="2x" />
-            </a>
-          </div>
-        </div>
-        <div className="rightBox">
-            <h1>About</h1>
-        </div>
-      </div>
+      <Router>
+            <div>
+              <Switch>
+                <Route path="/projects">
+                  <ProjectsPage />
+                </Route>
+                <Route path="/">
+                  <HomePage />
+                </Route>
+              </Switch>
+            </div>
+        </Router>
     </div>
   );
 }
