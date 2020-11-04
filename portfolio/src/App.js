15cbb6eb1f07eb60bc5de46d1669081799a15d5f
April Ma <54913304+april5622@users.noreply.github.com>
2020-11-03 16:39:14 -0800

    created a fading transition when routing to a new page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,30 +1,63 @@
-import React from 'react';
-import './App.css';
-import ProjectsPage from './components/ProjectsPage';
-import HomePage from './components/HomePage';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import ProjectsPage from "./components/ProjectsPage";
+import HomePage from "./components/HomePage";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import styled from "styled-components";
+import { Switch, Route, withRouter } from "react-router-dom";
 
-function App() {
+function App({ location }) {
   return (
-    <div className="App">
-      <Router>
-            <div>
-              <Switch>
-                <Route path="/projects">
-                  <ProjectsPage />
-                </Route>
-                <Route path="/">
-                  <HomePage />
-                </Route>
+    <Wrapper>
+      <TransitionGroup className="transition-group">
+        <CSSTransition
+          key={location.key}
+          timeout={{ enter: 300, exit: 300 }}
+          classNames={"fade"}
+        >
+          <div className="route-div">
+            <div className="App">
+              <Switch location={location}>
+                <Route path="/projects" component={ProjectsPage}/>
+                <Route path="/" component={HomePage}/>
               </Switch>
             </div>
-        </Router>
-    </div>
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
+    </Wrapper>
   );
 }
 
-export default App;
+const Wrapper = styled.div`
+  .fade-enter {
+    opacity: 0.01;
+  }
+
+  .fade-enter.fade-enter-active {
+    opacity: 1;
+    transition: opacity 300ms ease-in;
+  }
+
+  .fade-exit {
+    opacity: 1;
+  }
+
+  .fade-exit.fade-exit-active {
+    opacity: 0.01;
+    transition: opacity 300ms ease-in;
+  }
+
+  div.transition-group {
+    position: relative;
+  }
+
+  div.route-div {
+    position: absolute;
+    width: 100%;
+    top: 0;
+    left: 0;
+  }
+`;
+
+export default withRouter(App);
